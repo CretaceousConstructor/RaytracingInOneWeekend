85eda0f405806115bf90d5f335ac8e533b66b91c
CretaceousConstructor <397128933@qq.com>
2020-06-05 10:27:07 +0800

    added bunch of comments and shits

--- main.cpp
+++ main.cpp
@@ -1,26 +1,79 @@
-#include <iostream>
-#include "vec3.h"
+#include "rtweekend.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "color.h"
+#include "camera.h"
+color ray_color(const ray& r, const hittable& world, int depth) {
+	hit_record rec;
 
+	if (depth <= 0) {
+		return color(0.0, 0.0, 0.0);
+	}
+	// shadow acne problem. solved by limiting minmum value of t to 0.001
+	if (world.hit(r, 0.001, infinity, rec)) {
+		//diffuse rays generated!
+		point3 target = rec.p + rec.normal + random_unit_vector();
+		//diffuse rays generated!
+		//point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		//diffuse rays generated!
+		//point3 target = rec.p + random_in_hemisphere(rec.normal);
+		return 0.5 * ray_color(ray(rec.p, target - rec.p), world,depth - 1);
+		//more different the normals are,more different the colors  on piexls are
+		//return 0.5 * (rec.normal + color(1, 1, 1));
+	}
+
+	//using interpolation to generate background color or envirenment lighting
+	//if the ray didn't hit anything,background color will be returned;
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 
 
 
 int main() {
 
-	const int image_height = 100;
-	const int image_width = 200;
+	const auto aspect_ratio = (double)16 / (double)9;
+	const int image_width = 384;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	//     ---------
+	//     |       |
+	//     |       |
+	//     ---------
 
-    for (int j = image_height - 1; j >= 0; --j) {   //jmax  = 99
-        for (int i = 0; i < image_width; ++i) {     //imax = 199
-            auto r = double(i) / image_width;       //rmax = 0.995
-            auto g = double(j) / image_height;      //gmax = 0.99
-            auto b = 0.2;
-            int ir = static_cast<int>(255.999 * r); //ir max = 254
-            int ig = static_cast<int>(255.999 * g); //ig max = 253
-            int ib = static_cast<int>(255.999 * b); //ib     == 51
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0));
+
+
+	camera cam;
+
+	for (int j = image_height - 1; j >= 0; --j) {   //jmax  = 99   y axis goes up
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) {     //imax = 199
+			//   
+			//  ny(j)
+			//   |
+			//   |
+			//   |
+			//   ------>nx(i)
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				//// Returns a random real in [0,1). [i+0,i+1)
+				auto u = (i + random_double()) / ((double)image_width - 1.0);
+				auto v = (j + random_double()) / ((double)image_height - 1.0);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+
+			write_color(std::cout, pixel_color, samples_per_pixel);
+		}
+	}
+	std::cerr << "\nDone.\n";
 }
--- vec3.h
+++ vec3.h
@@ -6,46 +6,93 @@ class vec3
 {
 public:
 	vec3() = default;
-	vec3(float e0, float e1, float e2)
+	vec3(double e0, double e1, double e2)
 	{
 		e[0] = e0;
 		e[1] = e1;
 		e[2] = e2;
 	}
-	inline float x()const { return e[0]; };
-	inline float y()const { return e[1]; };
-	inline float z()const { return e[2]; };
-	inline float r()const { return e[0]; };
-	inline float g()const { return e[1]; };
-	inline float b()const { return e[2]; };
+	inline double x()const { return e[0]; };
+	inline double y()const { return e[1]; };
+	inline double z()const { return e[2]; };
+	inline double r()const { return e[0]; };
+	inline double g()const { return e[1]; };
+	inline double b()const { return e[2]; };
 	inline const vec3& operator+() const {
 		return *this;
 	}
 	inline  vec3 operator-() const {
 		return vec3(-e[0], -e[1], -e[2]);
 	}
-	inline float operator[](int i) const {
+	inline double operator[](int i) const {
 		return e[i];
 	}
-	inline float& operator[](int i) {
+	inline double& operator[](int i) {
 		return e[i];
 	}
 	inline vec3& operator+=(const vec3& v2);
 	inline vec3& operator-=(const vec3& v2);
 	inline vec3& operator*=(const vec3& v2);
 	inline vec3& operator/=(const vec3& v2);
-	inline vec3& operator*=(const float t);
-	inline vec3& operator/=(const float t);
-	inline float length() const {
+	inline vec3& operator*=(const double t);
+	inline vec3& operator/=(const double t);
+	inline double length() const {
 		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
-	inline float squared_length() const {
+	inline double squared_length() const {
 		return (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
 	inline void make_unit_vector();
-	float e[3];
+	double e[3];
+
+	static double dot(vec3 a, vec3 b) {
+		return (a.e[0] * b.e[0] + a.e[1] * b.e[1] + a.e[2] * b.e[2]);
+	}
+	vec3& saturate() {
+		if (e[0] > 1.0) { e[0] = 1.0; }
+		if (e[1] > 1.0) { e[1] = 1.0; }
+		if (e[2] > 1.0) { e[2] = 1.0; }
+		return *this;
+	}
+
+
+	inline static vec3 random() {
+		return vec3(random_double(), random_double(), random_double());
+	}
+
+	inline static vec3 random(double min, double max) {
+		return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+	}
+
+
 };
 
+
+
+
+
+inline vec3 random_in_unit_sphere() {
+	while (true) {
+		auto p = vec3::random(-1, 1);
+		if (p.squared_length() >= 1) continue;
+		return p;
+	}
+}
+vec3 random_unit_vector() {
+	auto a = random_double(0, 2 * pi);
+	auto z = random_double(-1, 1);
+	auto r = sqrt(1 - z * z);
+	//unit vector
+	return vec3(r * cos(a), r * sin(a), z);
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+	vec3 in_unit_sphere = random_in_unit_sphere();
+	if (vec3::dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+		return in_unit_sphere;
+	else
+		return -in_unit_sphere;
+}
 
 
 inline std::istream& operator>>(std::istream& is, vec3& t) {
@@ -56,9 +103,6 @@ inline std::ostream& operator<<(std::ostream& os, vec3& t) {
 	os << t.e[0] << " " << t.e[1] << " " << t.e[2];
 	return os;
 }
-
-
-
 
 inline vec3 operator+(const vec3& v1, const vec3& v2) {
 
@@ -76,11 +120,11 @@ inline vec3 operator/(const vec3& v1, const vec3& v2) {
 
 	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
-inline vec3 operator*(float t, const vec3& v) {
+inline vec3 operator*(double t, const vec3& v) {
 
 	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
-inline vec3 operator/(const vec3& v, float t) {
+inline vec3 operator/(const vec3& v, double t) {
 
 	return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
@@ -93,6 +137,7 @@ inline vec3& vec3::operator+=(const vec3& v2)
 	e[0] += v2.e[0];
 	e[1] += v2.e[1];
 	e[2] += v2.e[2];
+	return *this;
 }
 inline vec3& vec3::operator-=(const vec3& v2)
 {
@@ -112,7 +157,7 @@ inline vec3& vec3::operator/=(const vec3& v2)
 	e[1] /= v2.e[1];
 	e[2] /= v2.e[2];
 }
-inline vec3& vec3::operator*=(const float t) {
+inline vec3& vec3::operator*=(const double t) {
 
 	e[0] *= t;
 	e[1] *= t;
@@ -120,9 +165,9 @@ inline vec3& vec3::operator*=(const float t) {
 	return *this;
 
 }
-inline vec3& vec3::operator/=(const float t) {
+inline vec3& vec3::operator/=(const double t) {
 
-	float k = 1.0f / t;
+	double k = 1.0f / t;
 
 	e[0] *= k;
 	e[1] *= k;
@@ -131,13 +176,19 @@ inline vec3& vec3::operator/=(const float t) {
 }
 inline void vec3::make_unit_vector()
 {
-	float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	double k = 1.0f / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
 	e[0] *= k;
 	e[1] *= k;
 	e[2] *= k;
 
 }
 inline vec3 unit_vector(vec3 v) {
-
 	return v / v.length();
 }
+
+vec3 reflect(const vec3& v, const vec3& n) {
+	return v - 2 *vec3::dot(v, n) * n;
+}
+
+using point3 = vec3;   // 3D point
+using color = vec3;    // RGB color
